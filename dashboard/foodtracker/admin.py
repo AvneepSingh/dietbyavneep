91b1c3ab8039d23c5b2833e84604d82bca1a9b06
AvneepSingh <avneep10@gmail.com>
2023-04-07 23:03:43 +0530

    'version 7.1'

---
+++ dashboard/foodtracker/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+
+from .models import User, Food, FoodCategory, FoodLog, Image, Weight ,Coaches,CoachDP,UserCalories,Premium,Foodpref
+
+admin.site.register(User)
+admin.site.register(Food)
+admin.site.register(FoodCategory)
+admin.site.register(FoodLog)
+admin.site.register(Image)
+admin.site.register(Weight)
+admin.site.register(Coaches)
+admin.site.register(CoachDP)
+admin.site.register(UserCalories)
+admin.site.register(Premium)
+admin.site.register(Foodpref)
